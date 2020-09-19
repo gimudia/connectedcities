2f00cd1c0a41419be541ffebd5981c8a86e1c7cb
Godfrey Imudia <gimudia@hotmail.com>
2020-09-19 10:27:06 -0400

    initial commit with basic project setup

---
+++ src/main/java/com/codechallenge/road/city/connect/ConnectApplication.java
@@ -0,0 +1,17 @@
+package com.codechallenge.road.city.connect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Godfrey
+ *
+ */
+@SpringBootApplication
+public class ConnectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/codechallenge/road/city/connect/controller/CityConnectController.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.codechallenge.road.city.connect.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Godfrey
+ *
+ */
+@RestController
+@RequestMapping("/connected")
+public class CityConnectController {
+	private static final Logger LOGGER = LoggerFactory.getLogger(CityConnectController.class);
+
+}
